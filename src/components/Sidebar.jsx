6fb83a6c983ae33e0a3276d09d25870d9fefb508
ardiansetya <ardiansetya129@gmail.com>
2024-12-25 11:52:21 +0700

    create/sidebar-sticky

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,30 +14,35 @@ const Sidebar = () => {
   }
   return (
     <aside className=" w-64 min-h-screen  bg-slate-800 text-white flex flex-col rounded-br-xl">
-      <div className="p-12">
-        <h1 className="text-3xl font-bold">Data Mahasiswa</h1>
+      <div className="sticky top-5">
+        <div className="p-12 ">
+          <h1 className="text-3xl font-bold">Data Mahasiswa</h1>
+        </div>
+        <nav className="flex flex-col gap-4 px-4">
+          <a
+            href="#"
+            className="hover:bg-gray-700 px-4 py-2 rounded-md transition">
+            Dashboard
+          </a>
+          <a
+            href="#"
+            className="hover:bg-gray-700 px-4 py-2 rounded-md transition">
+            Data
+          </a>
+          <a
+            href="#"
+            className="hover:bg-gray-700 px-4 py-2 rounded-md transition">
+            Settings
+          </a>
+          {token ? (
+            <Button variant="danger" onClick={handleLogout}>
+              Logout
+            </Button>
+          ) : (
+            <Button onClick={handleLogout}>Login</Button>
+          )}
+        </nav>
       </div>
-      <nav className="flex flex-col gap-4 px-4">
-        <a
-          href="#"
-          className="hover:bg-gray-700 px-4 py-2 rounded-md transition">
-          Dashboard
-        </a>
-        <a
-          href="#"
-          className="hover:bg-gray-700 px-4 py-2 rounded-md transition">
-          Data
-        </a>
-        <a
-          href="#"
-          className="hover:bg-gray-700 px-4 py-2 rounded-md transition">
-          Settings
-        </a>
-        {
-          token ? <Button variant="danger" onClick={handleLogout}>Logout</Button>: <Button onClick={handleLogout}>Login</Button>
-
-        }
-      </nav>
     </aside>
   );
 };
